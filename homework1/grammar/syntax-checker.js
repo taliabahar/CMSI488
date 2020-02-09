ab4dbcd43f4ea2326750364730ed5bb497cb5c37
Sam gibson <samgibson@Sams-MacBook-Pro.local>
2020-02-08 18:23:45 -0800

    did we get rid of the grammar?

--- homework1/grammar/syntax-checker.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const ohm = require("ohm-js");
-
-const grammar = ohm.grammar(fs.readFileSync("grammar/Inkling.ohm"));
-
-module.exports = text => grammar.match(text).succeeded();
