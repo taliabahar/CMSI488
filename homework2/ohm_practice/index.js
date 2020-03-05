d49416ad0b76475601b0b11b808a8878a1475d46
coopslarhette <coopslarhette@gmail.com>
2020-03-04 20:00:42 -0800

    pleases eslint

--- homework2/ohm_practice/index.js
+++ homework2/ohm_practice/index.js
@@ -1,36 +1,36 @@
-const ohm = require("ohm-js");
+const ohm = require('ohm-js')
 
 function isCanadianPostalCode(s) {
-   const grammar = ohm.grammar(`CanadianPostalCode {
+  const grammar = ohm.grammar(`CanadianPostalCode {
       postalCode = ~("D" | "F" | "I" | "O" | "Q" | "U" | "W" | "Z") upper digit upper " " digit upper digit
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isVisa(s) {
-   const grammar = ohm.grammar(`VisaNumber {
+  const grammar = ohm.grammar(`VisaNumber {
       visaNumber = "4" digit digit digit digit digit digit digit digit digit digit digit digit (digit digit digit)?
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isMasterCard(s) {
-   const grammar = ohm.grammar(`MastercardNumber {
+  const grammar = ohm.grammar(`MastercardNumber {
       mastercardNumber = "5" "0" .. "5" digit digit digit digit digit digit digit digit digit digit digit digit digit digit
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isDivisibleBy64(s) {
-   const grammar = ohm.grammar(`IsDivisibleBy64 {
+  const grammar = ohm.grammar(`IsDivisibleBy64 {
       isDivisibleBy64 = (~("000000" end) ("0"|"1"))* "000000"   -- nonzero
                       | "0"+                                    -- zero
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isAdaFloat(s) {
-   const grammar = ohm.grammar(`AdaFloat {
+  const grammar = ohm.grammar(`AdaFloat {
       float = digits (frac | based)? exp?
       digits = digit ("_"? digit)*
       frac = "." digits
@@ -38,34 +38,34 @@ function isAdaFloat(s) {
       hexDigits = hexDigit ("_"? hexDigit)*
       exp = ("E" | "e") ("+" | "-")? digits
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isEightThroughTwentyNine(s) {
-    const grammar = ohm.grammar(`IsEightThroughTwentyNine {
+  const grammar = ohm.grammar(`IsEightThroughTwentyNine {
         isEightThroughTwentyNine = "8" | "9" | "10" | "11" | "12" | "13" | "14" | "15" | "16" | "17" | "18" | "19" | "20"
                                  | "21" | "22"|  "23" | "24" | "25" | "26" | "27" | "28" | "29"
     }`)
-    return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isMLComment(s) {
-   const grammar = ohm.grammar(`IsMLComment {
+  const grammar = ohm.grammar(`IsMLComment {
       comment = "(*"  (~("*)") (any))* "*)"
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isNotDogDoorDenWithLookAround(s) {
-   const grammar = ohm.grammar(`IsNotDogDoorDenWithLookAround {
+  const grammar = ohm.grammar(`IsNotDogDoorDenWithLookAround {
       isNotDogDoorDenWithLookAround = ~keyword  alnum*
       keyword                       = ("dog" | "door"  | "den") ~alnum
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 function isNotDogDoorDenNoLookAround(s) {
-   const grammar = ohm.grammar(`isNotDogDoorDenNoLookAround {
+  const grammar = ohm.grammar(`isNotDogDoorDenNoLookAround {
       comment = ("a".."c" | "e".."z" | "A".."Z") letter*                     --noD
               | "d" ("a".."d" | "f".."n" | "p".."z" | "A".."Z") letter*      --dWithoutOE
               | "do" ("a".."f" | "h".."n" | "p".."z" | "A".."Z") letter*     --doWithoutGO
@@ -78,18 +78,17 @@ function isNotDogDoorDenNoLookAround(s) {
               | letter letter letter letter letter+                          --bigWords
               | letter? letter?                                              --smolWords
    }`)
-   return grammar.match(s).succeeded();
+  return grammar.match(s).succeeded()
 }
 
 module.exports = {
-   isCanadianPostalCode,
-   isVisa,
-   isMasterCard,
-   isEightThroughTwentyNine,
-   isMLComment,
-   isDivisibleBy64,
-   isAdaFloat,
-   isNotDogDoorDenWithLookAround,
-   isNotDogDoorDenNoLookAround,
+  isCanadianPostalCode,
+  isVisa,
+  isMasterCard,
+  isEightThroughTwentyNine,
+  isMLComment,
+  isDivisibleBy64,
+  isAdaFloat,
+  isNotDogDoorDenWithLookAround,
+  isNotDogDoorDenNoLookAround,
 }
-
--- homework2/regex_practice/index.js
+++ homework2/regex_practice/index.js
@@ -1,33 +1,32 @@
 module.exports = {
-    isCanadianPostalCode(s) {
-      return /^(?![ DFIOQU]).*[A-Z][0-9][A-Z] [0-9][A-Z][0-9]$/.test(s)
-    },
-    isVisa(s) {
-      return /^4\d{3}(\d{4}){2}(\d{1,4})$/.test(s)
-    },
-    isMasterCard(s) {
-      return /^5[0-5]\d{2}(\d{4}){3}$/.test(s)
-    },
-    isAdaFloat(s) {
-      return /^(\d(_\d)?)+((\.(\d_?)+)?(([eE])[+-]?((\d_?)+)))?$|^(\d_?)+#\w+(.\w+)?#(([eE])-?((\d_?)+))?$/.test(s)
-    },
-    isDivisibleBy64(s) {
-      return /^1[0-1]*0{6}$|^0+$/.test(s)
-    },
-    isEightThroughTwentyNine(s) {
-      return /^([8-9]|(([12])\d))(\.(\d*))?$/.test(s)
-    },
-    isMLComment(s) {
-      return /^\(\*((?!\*\)).)*(\*\))$/.test(s)
-    },
-    isNotDogDoorDenWithLookAround(s) {
-      return /^(?!(dog|door|den)$)[a-zA-Z]*$/.test(s)
-    },
-    isNotDogDoorDenNoLookAround(s) {
-      return /^(d?o?o?e?|[^d][a-zA-z]*|[d][^(o|e)][[a-zA-z]*|[d][e][^n][[a-zA-z]*|[d][e][n][[a-zA-z]+|[d][o][^(g|o)][[a-zA-z]*|[d][o][g][[a-zA-z]+|[d][o][o][^r][[a-zA-z]*|[d][o][o][r][[a-zA-z]+)$/gm.test(s)
-    },
-    isNotThreeEndingInOO(s) {
-      return /^(?!^[a-z](oo)$)[a-z]*$/i.test(s)
-    },
-  }
-  
+  isCanadianPostalCode(s) {
+    return /^(?![ DFIOQU]).*[A-Z][0-9][A-Z] [0-9][A-Z][0-9]$/.test(s)
+  },
+  isVisa(s) {
+    return /^4\d{3}(\d{4}){2}(\d{1,4})$/.test(s)
+  },
+  isMasterCard(s) {
+    return /^5[0-5]\d{2}(\d{4}){3}$/.test(s)
+  },
+  isAdaFloat(s) {
+    return /^(\d(_\d)?)+((\.(\d_?)+)?(([eE])[+-]?((\d_?)+)))?$|^(\d_?)+#\w+(.\w+)?#(([eE])-?((\d_?)+))?$/.test(s)
+  },
+  isDivisibleBy64(s) {
+    return /^1[0-1]*0{6}$|^0+$/.test(s)
+  },
+  isEightThroughTwentyNine(s) {
+    return /^([8-9]|(([12])\d))(\.(\d*))?$/.test(s)
+  },
+  isMLComment(s) {
+    return /^\(\*((?!\*\)).)*(\*\))$/.test(s)
+  },
+  isNotDogDoorDenWithLookAround(s) {
+    return /^(?!(dog|door|den)$)[a-zA-Z]*$/.test(s)
+  },
+  isNotDogDoorDenNoLookAround(s) {
+    return /^(d?o?o?e?|[^d][a-zA-z]*|[d][^(o|e)][[a-zA-z]*|[d][e][^n][[a-zA-z]*|[d][e][n][[a-zA-z]+|[d][o][^(g|o)][[a-zA-z]*|[d][o][g][[a-zA-z]+|[d][o][o][^r][[a-zA-z]*|[d][o][o][r][[a-zA-z]+)$/gm.test(s)
+  },
+  isNotThreeEndingInOO(s) {
+    return /^(?!^[a-z](oo)$)[a-z]*$/i.test(s)
+  },
+}
